b22eef1f87ea0f6a6cb0b6a0757400901911b576
Rick Mansfield <PEACEFULRICK@msn.com>
2022-01-31 11:41:20 -0600

    Add project files.

---
+++ ForLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
